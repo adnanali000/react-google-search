e174e4dc150c852c30bd2e745cb4c1dd947cf193
adnanali000 <adnan.museel123@gmail.com>
2022-04-02 15:09:58 +0500

    installing dependencies like react-router-dom use-debounce react-loader-spinner and react-player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Navbar} from './components/Navbar'
+import {Footer} from './components/Footer'
+import {Routes} from './components/Routes'
+
 
 const App = () => {
+    const [darkTheme,setDarkTheme] = useState(false);
   return (
-    <div>
-        <h1>Google clone</h1> 
-    </div>
+   <div className={darkTheme ? 'dark' : ''}>
+       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+           <Navbar />
+           <Routes />
+           <Footer />
+       </div>
+
+   </div>
   )
 }
 
