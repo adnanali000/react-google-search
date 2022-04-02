387e89478a16cd655fcb9062c61202aeab81fda1
adnanali000 <adnan.museel123@gmail.com>
2022-04-02 16:09:45 +0500

    route work done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React,{useState} from 'react'
-import {Navbar} from './components/Navbar'
-import {Footer} from './components/Footer'
-import {Routes} from './components/Routes'
+import React, { useState } from 'react'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
+import { Results } from './components/Results';
 
 
 const App = () => {
-    const [darkTheme,setDarkTheme] = useState(false);
-  return (
-   <div className={darkTheme ? 'dark' : ''}>
-       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
-           <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-           <Routes />
-           <Footer />
-       </div>
+    const [darkTheme, setDarkTheme] = useState(false);
+    return (
+        <Router>
+            <div className={darkTheme ? 'dark' : ''}>
+                <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                    <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                    <Routes>
+                        <Route path='/' element={<Navigate to="/search" />} />
+                        <Route path='/search' element={<Results />} />
+                        <Route path='/images' element={<Results />} />
+                        <Route path='/videos' element={<Results />} />
+                        <Route path='/news' element={<Results />} />
+                    </Routes>
+                    <Footer />
+                </div>
 
-   </div>
-  )
+            </div>
+        </Router>
+    )
 }
 
 export default App
